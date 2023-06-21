41055c22a0f4ac5573588e97cabcfd6a22a2b6e8
SyahrinNanda <syahrinnanda@gmail.com>
2023-06-22 01:00:28 +0800

    add file

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "As'syahrin Nanda"; // TODO: replace this
+const studentId = "FE5130073"; // TODO: replace this
+
+export { url, studentName, studentId };
